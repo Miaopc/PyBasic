51fe41f723601b593dc0e6fb1515a7419166c689
Miaopc <1651237104@qq.com>
2018-02-20 09:09:06 +0800

    example1

---
+++ example/dtype1.py
@@ -0,0 +1,13 @@
+str='hello world！'
+
+print (str)
+ # Prints complete string
+print (str[0])
+ # Prints first character of the string
+print (str[2:5])
+ # Prints characters starting from 3rd to 5th
+print (str[2:])
+ # Prints string starting from 3rd character
+print (str * 3)
+ # Prints string three times
+print (str + 'YOYO') # Prints concatenated string
